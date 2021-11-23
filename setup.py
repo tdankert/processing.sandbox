0c5ee4464f9ed9a48a08096bc2a407a8215892c1
Timo Dankert <tdankert@slashwhy.de>
2021-11-23 13:01:27 +0100

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pythonProject2',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='tdankert',
+    author_email='timo.dankert@posteo.net',
+    description=''
+)
